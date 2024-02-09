9597b24e7e4b5acddace759c0a37112771b9a1a9
Kibble White <kibblewhite@live.com>
2024-02-09 11:20:37 +0100

    Minor

--- EditorJsonToHtml.Lib/Renderers/IBlockRenderer.cs
+++ EditorJsonToHtml.Lib/Renderers/IBlockRenderer.cs
@@ -5,10 +5,4 @@ namespace EditorJsonToHtml.Lib.Renderers;
 public interface IBlockRenderer
 {
     static abstract void Render(CustomRenderTreeBuilder render_tree_builder, EditorJsBlock block);
-
-    static virtual void Resize(int width, int height)
-    {
-        // Default implementation for Resize method
-        // Code to resize the shape
-    }
 }
